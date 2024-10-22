035417c1fd3302af9bde6480f308180402621b45
“Michal <michal.gosek@4chain.studio>
2024-10-22 12:41:09 +0200

    Added query params filtering example.

--- example/api/v1/user/transactions/transactions/transactions_example.go
+++ example/api/v1/user/transactions/transactions/transactions_example.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	client "github.com/bitcoin-sv/spv-wallet-go-client"
 	"github.com/bitcoin-sv/spv-wallet-go-client/example/exampleutil"
+	"github.com/bitcoin-sv/spv-wallet-go-client/internal/api/v1/user/transactions"
+	"github.com/bitcoin-sv/spv-wallet/models/filter"
 )
 
 func main() {
@@ -22,4 +25,13 @@ func main() {
 		log.Fatal(err)
 	}
 	exampleutil.Print("HTTP api/v1/transactions - all transactions", tt)
+
+	bh := uint64(865365)
+	t, err := spv.Transactions(ctx, transactions.WithTransactionFilter(filter.TransactionFilter{
+		BlockHeight: &bh,
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	exampleutil.Print(fmt.Sprintf("HTTP api/v1/transactions filtered - all transactions with block height: %d", bh), t)
 }
